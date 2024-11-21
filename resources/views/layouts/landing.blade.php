d401b682228a7dc4fdf82335e9bafaf24839050c
AdrianID <adrianfrizna14@gmail.com>
2024-11-21 10:56:57 +0700

    afdfad

--- resources/views/layouts/landing.blade.php
+++ resources/views/layouts/landing.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     {{-- <title>@yield('title') | Lontar Sewu</title> --}}
     {{-- <title>@yield('title') | Lontar Sewu</title> --}}
-    @vite('resources/css/app.css')
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     @yield('css')
 </head>
 <body class="bg-[#FFF1D6]">
